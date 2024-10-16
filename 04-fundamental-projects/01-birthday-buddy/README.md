cb3a7d9d1630565cf7aff93c1ae071bcd3bef8a4
tphilus <tkdadzie@st.ug.edu.gh>
2024-10-16 10:35:19 +0000

    done with birthday-buddy

--- 04-fundamental-projects/01-birthday-buddy/README.md
+++ 04-fundamental-projects/01-birthday-buddy/README.md
@@ -1,5 +1,4 @@
 ## Figma URL
-
 [Birthday Buddy](https://www.figma.com/file/e2vsLe9DMnXZIygNHkwGL1/Birthday-buddy?node-id=0%3A1&t=AGNWdO5QQGOoNCfD-1)
 
 ## Steps
--- 04-fundamental-projects/01-birthday-buddy/src/App.jsx
+++ 04-fundamental-projects/01-birthday-buddy/src/App.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+import data from './data'
+import List from "./components/List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  // console.log("People", people.length);
+
+  const clearList = () => {
+    setPeople([])
+  }
+  
+  return <main>
+    <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} />
+      <button type="button" className="btn btn-block" onClick={clearList} >Clear all</button>
+    </section>
+  </main>
 };
 export default App;
