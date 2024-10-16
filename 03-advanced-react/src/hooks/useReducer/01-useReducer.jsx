bfc0ae1e0ceb076da1f3c5241fea1074b0144652
tphilus <tkdadzie@st.ug.edu.gh>
2024-10-16 06:39:01 +0000

    update

--- 03-advanced-react/src/hooks/useReducer/01-useReducer.jsx
+++ 03-advanced-react/src/hooks/useReducer/01-useReducer.jsx
@@ -1,44 +1,29 @@
-import React, { useReducer } from "react";
+import React, { act, useReducer } from "react";
 import { data, people } from "../../data";
+import { REMOVE_LIST, CLEAR_LIST, RESET_LIST } from "./action";
+import reducer from "./reducer";
 
 const defaultState = {
   people: data,
   isLoading: false,
 };
 
-const CLEAR_LIST = 'CLEAR_LIST';
-const RESET_LIST = 'RESET_LIST';
-const REMOVE_LIST = 'REMOVE_LIST';
 
-
-const reducer = (state, action) => {
-  if (action.type === 'CLEAR_LIST') {
-    return {...state, people: []}
-  }
-
-  // return state;  
-  throw new Error(`No matching "${action.type}" - action type`)
-}
 
 const ReducerBasics = () => {
- const [state, dispatch] = useReducer(reducer, defaultState);
-console.log(state);
-
-
-  // const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    // let newPeople = people.filter((person) => person.id !== id);
-    // setPeople(newPeople);
+    dispatch({ type: REMOVE_LIST, payload: { id } });
   };
 
   const clearList = () => {
-    dispatch({type: 'CLEAR_LIST'});
-  }
+    dispatch({ type: "CLEAR_LIST" });
+  };
 
   const resetList = () => {
-    dispatch({type: 'RESET_LIST'})
-  }
+    dispatch({ type: "RESET_LIST" });
+  };
 
   return (
     <div>
@@ -51,11 +36,7 @@ console.log(state);
           </div>
         );
       })}
-      <button
-        className="btn"
-        style={{ marginTop: "2rem" }}
-        onClick={clearList}
-      >
+      <button className="btn" style={{ marginTop: "2rem" }} onClick={clearList}>
         clear items
       </button>
     </div>
