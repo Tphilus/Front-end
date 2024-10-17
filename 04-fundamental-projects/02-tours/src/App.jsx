4704991a1a2a614327cd1f9ca7078d256ddba92c
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2024-10-17 07:12:02 +0000

    Update App.jsx
    add no tour

--- 04-fundamental-projects/02-tours/src/App.jsx
+++ 04-fundamental-projects/02-tours/src/App.jsx
@@ -42,7 +42,26 @@ const App = () => {
     );
   }
 
-  TODO: return (
+  TODO:
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className=" title">
+          <h2>no tours left</h2>
+          <button
+            type="button"
+            className=" btn"
+            style={{ marginTop: "2rem" }}
+            onClick={() => fetchData()}
+          >
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  
+  return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
     </main>
