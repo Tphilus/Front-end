abf95257e4133e2f5236c49e62365e08cb732bf6
tphilus <tkdadzie@st.ug.edu.gh>
2024-10-22 06:59:23 +0000

    final commit on the menu Project

--- 04-fundamental-projects/05-menu/src/App.jsx
+++ 04-fundamental-projects/05-menu/src/App.jsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+import Title from "./components/Title";
+import menu from "./data";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+
+// const tempCategories = menu.map((item) => item.category)
+// const tempSet = new Set(tempCategories);
+// const tempItems = ['all', ...tempSet]
+// console.log(tempItems);
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItem, setMenuItem] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    console.log(category);
+    if( category === "all") {
+      setMenuItem(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category)
+    setMenuItem(newItems);
+  }
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="Our Menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItem} />
+      </section>
+    </main>
+  );
 };
 export default App;
