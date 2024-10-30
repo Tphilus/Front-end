da78332e5eb6a014d79723660739a2dd7fe1c11f
tphilus <tkdadzie@st.ug.edu.gh>
2024-10-30 11:59:30 +0000

    update

--- 04-fundamental-projects/08-lorem-ipsum/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/src/App.jsx
@@ -1,4 +1,50 @@
+import { useState } from "react";
+import data from "./data";
+import {nanoid} from 'nanoid'
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const amount = parseInt(count);
+    setText(data.slice(0, amount));
+  } 
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring Lorem Ipsum?</h4>
+      <form className="lorem-form" onClick={handleSubmit} >
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {
+          text.map((item) => {
+            return <p key={nanoid()} > {item} </p>
+          })
+        }
+        {/* {
+          text.map((item, index) => {
+            return <p key={index} > {item} </p>
+          })
+        } */}
+      </article>
+    </section>
+  );
 };
 export default App;
