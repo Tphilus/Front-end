d71988799e6d40ef0b15c02c0607fe4fae2af823
tphilus <tkdadzie@st.ug.edu.gh>
2024-11-03 18:24:08 +0000

    add: toast, can add, edit and delete item

--- 04-fundamental-projects/10-grocery-bud/src/App.jsx
+++ 04-fundamental-projects/10-grocery-bud/src/App.jsx
@@ -2,13 +2,26 @@ import { useState } from "react";
 import Form from "./components/Form";
 import { nanoid } from "nanoid";
 import Items from "./components/Items";
+import { toast, ToastContainer } from "react-toastify";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("list");
+  if (list) {
+    list = JSON.parse(localStorage.getItem("list"));
+  } else {
+    list = [];
+  }
+  return list;
+};
 
 const setLocalStorage = (items) => {
-  localStorage.setItem('list', JSON.stringify(items))
-}
+  localStorage.setItem("list", JSON.stringify(items));
+};
 
 const App = () => {
-  const [items, setItems] = useState([]);
+  // const [items, setItems] = useState(getLocalStorage());
+  const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -16,18 +29,37 @@ const App = () => {
       completed: false,
       id: nanoid(),
     };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("item added to the list");
   };
 
   const removeItem = (itemId) => {
     const newItem = items.filter((item) => item.id !== itemId);
-    setItems(newItem)
+    setItems(newItem);
+    setLocalStorage(newItem);
+    toast.success("item deleted");
+  };
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success("item added to the list");
   };
 
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
   );
 };
