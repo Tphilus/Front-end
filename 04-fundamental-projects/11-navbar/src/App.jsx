527438116c602bdb58f8dd0276ae0882d0cb7165
tphilus <tkdadzie@st.ug.edu.gh>
2024-11-04 18:09:13 +0000

    add: 11-navbar project

---
+++ 04-fundamental-projects/11-navbar/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+import {FaBars} from 'react-icons/fa';
+import {links,social} from './data';
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+};
+export default App;
