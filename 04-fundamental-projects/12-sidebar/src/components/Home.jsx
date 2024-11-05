eea20d70c1a84bc8b28c0f6c25ad3fb0425d7bef
tphilus <tkdadzie@st.ug.edu.gh>
2024-11-05 17:01:10 +0000

    add: 12-sidebar and 13-strapi-submenus

---
+++ 04-fundamental-projects/12-sidebar/src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useGlobalContext } from "../context";
+import { FaBars } from "react-icons/fa";
+
+const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+  console.log(openModal);
+
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
---
+++ 04-fundamental-projects/12-sidebar/src/context.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
