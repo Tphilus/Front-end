aa034f6b0bcf12501f64f4cf190a687991c7e049
tphilus <tkdadzie@st.ug.edu.gh>
2024-11-15 22:31:47 +0000

    update

--- 06-react-query/client/src/Items.jsx
+++ 06-react-query/client/src/Items.jsx
@@ -5,7 +5,7 @@ const Items = ({ items }) => {
   const { isLoading, data, error, isError } = useQuery({
     queryKey: ["tasks"],
     queryFn: async () => {
-      const { data } = await customFetch.get("/something");
+      const { data } = await customFetch.get("/");
       return data;
     },
   });
