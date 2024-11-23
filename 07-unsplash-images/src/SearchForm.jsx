375b3afd22024ee93735288c4a1aa54eb6d845ff
Tphilus <tkdadzie@st.ug.edu.gh>
2024-11-23 19:14:04 +0000

    add: contaxt and dark and light themes

--- 07-unsplash-images/src/SearchForm.jsx
+++ 07-unsplash-images/src/SearchForm.jsx
@@ -1,11 +1,27 @@
-
-
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if(!searchValue) return;
+    console.log(searchValue);
+    
+  };
   return (
-    <div>
-      SearchForm
-    </div>
-  )
-}
+    <section>
+      <h1 className="title">unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cart"
+        />
+        <button type="submit" className=" btn ">
+          search
+        </button>
+      </form>
+    </section>
+  );
+};
 
-export default SearchForm
+export default SearchForm;
--- 07-unsplash-images/src/ThemeToggle.jsx
+++ 07-unsplash-images/src/ThemeToggle.jsx
@@ -1,11 +1,19 @@
-
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import { useGlobalContext } from "./context";
 
 const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
   return (
-    <div>
-      ThemeToggle
-    </div>
-  )
-}
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme} >
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon" />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
