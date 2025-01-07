3671c3726cf9386c50f6964d55a52c8dd0a70b87
Tphilus <tkdadzie@st.ug.edu.gh>
2025-01-07 16:08:04 +0000

    add the route and others

---
+++ 09-mixmaster/src/pages/Error.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/not-found.svg";
+import { Link, useRouteError } from "react-router-dom";
+const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh!</h3>
+          <p>W can't seem to find the page you are looking for</p>
+          <Link to="/">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div>
+        <h2>something went wrong</h2>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Error;
