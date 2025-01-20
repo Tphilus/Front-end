6ea4abe7d6ea14a672ee047453440a7fa209a918
Tphilus <tkdadzie@st.ug.edu.gh>
2025-01-20 22:36:30 +0000

    update

--- 09-mixmaster/src/pages/Cocktail.jsx
+++ 09-mixmaster/src/pages/Cocktail.jsx
@@ -3,6 +3,11 @@ import axios from "axios";
 import Wrapper from "../assets/wrappers/CocktailPage";
 const singleCocktailUrl =
   "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+export const loader = async (data) => {
+  console.log(data);
+  return null;
+};
 const Cocktail = () => {
   return <div>Cocktail</div>;
 };
