271b83c7bbf079070ac7e9174b6cb08bd67cdcf3
Tphilus <tkdadzie@st.ug.edu.gh>
2025-03-13 18:25:15 +0000

    add: the reducers

--- 11-redux-toolkit-tutorial/src/features/cart/cartSlice.js
+++ 11-redux-toolkit-tutorial/src/features/cart/cartSlice.js
@@ -12,8 +12,27 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+      // return { cartItems: [] };
+    },
+    removeItem: (state, actions) => {
+      const itemId = actions.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
+  },
 });
 
 // console.log(cartSlice);
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
