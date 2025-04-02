b95ff4c92312debc40d3b574c768036947c2bdcc
Tphilus <tkdadzie@st.ug.edu.gh>
2025-04-02 20:47:59 +0000

    update

---
+++ 12-comfy-store/src/components/FormSelect.jsx
@@ -0,0 +1,9 @@
+const FormSelect = ({ label, name, list, defaultValue, size }) => {
+  return (
+    <div className="form-control">
+      <label htmlFor={name} className="label"></label>
+    </div>
+  );
+};
+
+export default FormSelect;
