c77992f1835b5a6ad3389886c5beddd433b1e585
Tphilus <tkanateydadzie@gmail.com>
2025-05-29 11:36:40 -0700

    add typescript folder

---
+++ typescript/src/Tutorial/typeAndInterface.ts
@@ -0,0 +1,30 @@
+let awesomeName: string = "ShakeAndBake";
+awesomeName = "something";
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+// awesomeName = 20
+let amount: number = 20;
+amount = 12 - 1;
+console.log(amount);
+
+let isAwesome: boolean = true;
+isAwesome = false;
+
+// exercise
+
+let greeting: string = "Hello, world";
+greeting = greeting.toUpperCase()
+
+let age: number = 25;
+age += 5
+
+let isAdault: boolean = age > 18;
+isAdault = !isAdault
+console.log(greeting, age, isAdault);
+
+
+// typeScript Errors 
+// greeting = 10
+// age = "thirty"
+// isAdault = 'yes'
---
+++ typescript/src/Tutorial/unionAndAnyType.ts
@@ -0,0 +1,35 @@
+let tax: number | string = 10;
+tax = 100;
+tax = '$10'
+
+let requestStatus: 'pending' | 'error' | 'success' = 'pending';
+requestStatus = 'error';
+requestStatus = 'pending';
+requestStatus = 'success';
+
+let notSure: any = 50;
+notSure = 'maybe a string instead';
+notSure = false;
+
+const books = ["1984", "Brave New World", "Fahrenheit 451"];
+
+let foundBook: string;
+
+for (let book of books) {
+  if (book === "Fahrenheit 451") {
+    foundBook = book;
+    break;
+  }
+}
+
+// console.log(foundBook);
+
+// Exercise 
+
+// 1. Order Status 
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+orderStatus = 'processing';
+
+// 2. Discount 
+let discount: number | string = 20;
+discount = '20%'
