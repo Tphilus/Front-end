ffb4910fc527b9d8d36300eaa22153510bbea296
Tphilus <tkanateydadzie@gmail.com>
2025-07-14 22:27:58 +0000

    add:productGrid components and card

--- 16-store/components/products/ProductGrid.tsx
+++ 16-store/components/products/ProductGrid.tsx
@@ -1,9 +1,49 @@
+"use client";
+import { formatCurrency } from "@/utils/format";
 import { Product } from "@prisma/client";
+import Link from "next/link";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
+import FavoriteToggleButton from "./FavoriteToggleButton";
 
 function ProductGrid({ products }: { products: [] }) {
   return (
     <div className=" pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      ProductGrid
+      {products.map((product) => {
+        const { id, name, price, image } = product;
+        const productId = id;
+        const dollarsAmount = formatCurrency(price);
+
+        return (
+          <article key={productId} className="group relative">
+            <Link href={`/products/${productId}`}>
+              <Card className=" transform group-hover:shadow-xl transition-shadow duration-500">
+                <CardContent className=" p-4">
+                  <div className="relative h-64 md:h-48 rounded overflow-hidden">
+                    <Image
+                      src={image}
+                      alt={name}
+                      fill
+                      sizes="(max-width:768px) 100vw, (max-width: 1200px) 50vw"
+                      priority
+                      className=" w-full rounded object-cover transform group-hover:scale-110 transition-transform duration-500"
+                    />
+                  </div>
+                  <div className=" mt-4 text-center">
+                    <h2 className="text-lg capitalize">{name}</h2>
+                    <p className=" text-muted-foreground mt-2">
+                      {dollarsAmount}
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
+            <div className="absolute top-12 right-7 z-5">
+              <FavoriteToggleButton productId={productId} />
+            </div>
+          </article>
+        );
+      })}
     </div>
   );
 }
